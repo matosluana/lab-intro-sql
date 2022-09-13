73f9638f40a64335f1729d43db524da72c2390e8
Luana <luanaprm@gmail.com>
2022-09-13 17:26:28 +0200

    Intro SQL Lab

---
+++ Intro-Lab.sql
@@ -0,0 +1,7 @@
+SELECT * FROM actor,film film, customer;
+SELECT title FROM film;
+SELECT DISTINCT * FROM language;
+SELECT DISTINCT store FROM sales_by_store;
+SELECT count(store_id) from store; -- alternative
+SELECT count(ID) from staff_list;
+SELECT first_name FROM staff;
